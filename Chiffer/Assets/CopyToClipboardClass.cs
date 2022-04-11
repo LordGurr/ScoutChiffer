33e8ed6eec7e30cc75abf2ffb5aee2926405dbbc
LordGurr <50996232+LordGurr@users.noreply.github.com>
2022-04-11 22:51:26 +0200

    ny init då förra inte funkade

---
+++ Chiffer/Assets/CopyToClipboardClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CopyToClipboardClass : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI myText;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void StartCopy()
+    {
+        CopyToClipboard(myText.text);
+    }
+
+    public void CopyToClipboard(string s)
+    {
+        TextEditor te = new TextEditor();
+        te.text = s;
+        te.SelectAll();
+        te.Copy();
+    }
+}
