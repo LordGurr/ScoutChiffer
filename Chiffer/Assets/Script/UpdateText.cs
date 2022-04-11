458ff9791277ce3bb9a5960af1f01cda51acb123
LordGurr <50996232+LordGurr@users.noreply.github.com>
2022-04-11 21:15:24 +0200

    Initial commit

---
+++ Chiffer/Assets/Script/UpdateText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateText : MonoBehaviour
+{
+    private TextMeshProUGUI myText;
+
+    [SerializeField] private RectTransform bakgrund;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myText = gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void TextUpdated(string input)
+    {
+        myText.text = input;
+        StartCoroutine(UppdateraNästaFrame());
+    }
+
+    private IEnumerator UppdateraNästaFrame()
+    {
+        yield return null;
+        bakgrund.sizeDelta = gameObject.GetComponent<RectTransform>().sizeDelta;
+    }
+}
---
+++ Chiffer/Assets/pagod.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pagod : MonoBehaviour
+{
+    public int indexAvDelEtt = -1;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void SetIndex(int indexDeltEtt)
+    {
+        indexAvDelEtt = indexDeltEtt;
+    }
+
+    public char GetLetter(int indexDelTvå)
+    {
+        if (indexAvDelEtt == 0)
+        {
+            return (char)indexDelTvå;
+        }
+        else if (indexAvDelEtt == 1)
+        {
+            if (indexDelTvå < 81)
+            {
+                return (char)indexDelTvå;
+            }
+            else
+            {
+                return (char)(indexDelTvå + 1);
+            }
+        }
+        else if (indexAvDelEtt == 2)
+        {
+            if (indexDelTvå < 86)
+            {
+                return (char)(indexDelTvå + 1);
+            }
+            else if (indexDelTvå > 88)
+            {
+                if (indexDelTvå == 89)
+                {
+                    return 'Å';
+                }
+                else if (indexDelTvå == 90)
+                {
+                    return 'Ä';
+                }
+                else
+                {
+                    return 'Ö';
+                }
+            }
+            else
+            {
+                return (char)(indexDelTvå + 2);
+            }
+        }
+        return (char)0;
+    }
+}
