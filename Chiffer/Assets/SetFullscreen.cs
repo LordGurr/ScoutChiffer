f1a0489f03490503cd2c761ba57a69aaffe43a07
LordGurr <50996232+LordGurr@users.noreply.github.com>
2022-04-18 16:18:59 +0200

    städade

---
+++ Chiffer/Assets/SetFullscreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFullscreen : MonoBehaviour
+{
+    [SerializeField] private bool fullscreen;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Screen.fullScreen = fullscreen;
+    }
+}
